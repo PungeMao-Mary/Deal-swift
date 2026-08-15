dea34d8192b1874e35621bf15b81af1f58beb7a8
pungemaomary <tiffiny.xia@gmail.com>
2026-08-15 13:11:44 +0800

    Initial Commit

---
+++ Deal/DealApp.swift
@@ -0,0 +1,17 @@
+//
+//  DealApp.swift
+//  Deal
+//
+//  Created by Tiffany Xia on 2026/08/15.
+//
+
+import SwiftUI
+
+@main
+struct DealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
